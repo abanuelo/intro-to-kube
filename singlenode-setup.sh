3a462dc14cbb64165fe52fa869658b1446317fbd
Armando Banuelos <abanuelo@stanford.edu>
2023-06-20 20:51:32 -0700

    Adding scavenger hunt

--- singlenode-setup.sh
+++ singlenode-setup.sh
@@ -3,3 +3,4 @@ if [ ! -f /usr/local/bin/minikube ]; then
   echo "Minikube not found. Please follow installation instructions in README.md"
 fi
 minikube start
+kubectl create -f ./scavenger-hunt/k8s-specifications
