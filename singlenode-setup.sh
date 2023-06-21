2b914d4e5a593789efb9a0b599072d9f592d8f39
Armando Banuelos <abanuelo@stanford.edu>
2023-06-20 22:42:34 -0700

    Deleting certain files

--- singlenode-setup.sh
+++
@@ -1,6 +0,0 @@
-# Run only once minikube and relevant dependencies installed
-if [ ! -f /usr/local/bin/minikube ]; then
-  echo "Minikube not found. Please follow installation instructions in README.md"
-fi
-minikube start
-kubectl create -f ./scavenger-hunt/k8s-specifications
